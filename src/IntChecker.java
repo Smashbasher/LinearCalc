046f7238e7a2d9bbd2e32bdca2996d3b6ff7225a
Paul <106315598+Smashbasher@users.noreply.github.com>
2022-06-22 03:12:36 +0300

    Delete IntChecker.java

--- src/IntChecker.java
+++
@@ -1,8 +0,0 @@
-public class IntChecker {
-    public static boolean check(String[] values) throws Exception {
-        int intValue = 0;
-        if (intValue != Integer.parseInt(values[0])) {
-            throw new Exception("Вводимое выражение должно начинаться со строки");
-        }else return true;
-    }
-}
